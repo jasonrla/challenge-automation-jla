e45f02a5dc44c7b1583817918184cc8faa181ccb
jasonrla <jasonrla@gmail.com>
2025-07-21 00:49:17 -0500

    update loadEnv.ts

--- utils/loadEnv.ts
+++ utils/loadEnv.ts
@@ -2,11 +2,12 @@ import * as dotenv from "dotenv";
 import * as path from "path";
 import * as fs from "fs";
 
+const isCI = process.env.CI === "true";
 const env = process.env.ENV || "qa";
 const envFile = `.env.${env}`;
 const envPath = path.resolve(process.cwd(), envFile);
 
-if (fs.existsSync(envPath)) {
+if (!isCI && fs.existsSync(envPath)) {
   dotenv.config({
     path: envPath,
     quiet: true,
